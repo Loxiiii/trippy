fbba72dfeca41e42cdb9c64f5410043e70129e01
Javi Campos Castello <javi.camposcastello@ul.com>
2024-10-31 14:49:39 -0500

    Added trip controller function

--- utils/controllers/tripController.ts
+++ utils/controllers/tripController.ts
@@ -1,3 +1,16 @@
-let getTripById = (tripId: number): => {
+import { Trip } from '@/utils/types'
 
-}
+export const getTripById = async (tripId: number, supabase: any): Promise<Trip | null> => {
+    const { data, error } = await supabase
+      .from('trips')
+      .select('*')
+      .eq('id', tripId)
+      .single(); // Assuming you want a single trip
+  
+    if (error) {
+      console.error('Error fetching trip:', error);
+      return null;
+    }
+  
+    return data;
+  };
